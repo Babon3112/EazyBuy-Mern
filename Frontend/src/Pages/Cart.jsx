ad3d4584469e15c7f26714ea92b5e3e6122c5466
Arnab Ghosh <arnabbabon2002@gmail.com>
2024-04-06 19:55:42 +0530

    finished cart page

---
+++ Frontend/src/Pages/Cart.jsx
@@ -0,0 +1,248 @@
+import React from "react";
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import Announcements from "../components/Announcements";
+import Footer from "../components/Footer";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  font-weight: 400;
+  text-align: center;
+`;
+
+const Top = styled.div`
+  display: flex;
+  padding: 20px;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const TopButton = styled.button`
+  padding: 10px;
+  font-weight: 600;
+  cursor: pointer;
+  border: ${(props) => props.type === "filled" && "none"};
+  background-color: ${(props) =>
+    props.type === "filled" ? "black" : "transparent"};
+  color: ${(props) => props.type === "filled" && "white"};
+`;
+
+const TopTexts = styled.span``;
+
+const TopText = styled.span`
+  text-decoration: underline;
+  cursor: pointer;
+  margin: 0 10px;
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Info = styled.div`
+  flex: 3;
+`;
+
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ProductDetails = styled.div`
+  display: flex;
+  flex: 2;
+`;
+
+const Image = styled.img`
+  width: 200px;
+`;
+
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const ProductName = styled.div``;
+
+const ProductID = styled.div``;
+
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
+
+const ProductSize = styled.div``;
+
+const PriceDetail = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+`;
+
+const ProductPrice = styled.div`
+  font-size: 30px;
+  font-weight: 200;
+`;
+
+const Hr = styled.hr`
+  height: 1px;
+  background-color: #eee;
+  border: none;
+  margin: 10px 0;
+`;
+
+const Summary = styled.div`
+  flex: 1;
+  border: 0.5px solid lightgrey;
+  border-radius: 10px;
+  padding: 20px;
+  height: 50vh;
+`;
+const SummaryTitle = styled.h1`
+  font-size: 30px;
+  font-weight: 200;
+`;
+
+const SummaryItem = styled.div`
+  margin: 30px 0;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
+`;
+
+const SummaryItemTexts = styled.span``;
+
+const SummaryItemPrice = styled.span``;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  font-weight: 600;
+`;
+
+const Cart = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcements />
+      <Wrapper>
+        <Title>Your Bag</Title>
+        <Top>
+          <TopButton>Continue Shopping</TopButton>
+          <TopTexts>
+            <TopText>Shooping Bag(2)</TopText>
+            <TopText>Your Wishlist(0)</TopText>
+          </TopTexts>
+          <TopButton type="filled">ChechOut Now</TopButton>
+        </Top>
+        <Bottom>
+          <Info>
+            <Product>
+              <ProductDetails>
+                <Image src="https://w7.pngwing.com/pngs/76/939/png-transparent-t-shirt-cambric-denim-jacket-sitting-boy-thumbnail.png" />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b>ABC
+                  </ProductName>
+                  <ProductID>
+                    <b>ID:</b>123
+                  </ProductID>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>Size:</b>L
+                  </ProductSize>
+                </Details>
+              </ProductDetails>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <RemoveIcon />
+                  <ProductAmount>2</ProductAmount>
+                  <AddIcon />
+                </ProductAmountContainer>
+                <ProductPrice>₹799</ProductPrice>
+              </PriceDetail>
+            </Product>
+            <Hr />
+            <Product>
+              <ProductDetails>
+                <Image src="https://w7.pngwing.com/pngs/76/939/png-transparent-t-shirt-cambric-denim-jacket-sitting-boy-thumbnail.png" />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b>ABC
+                  </ProductName>
+                  <ProductID>
+                    <b>ID:</b>123
+                  </ProductID>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>Size:</b>L
+                  </ProductSize>
+                </Details>
+              </ProductDetails>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <RemoveIcon />
+                  <ProductAmount>2</ProductAmount>
+                  <AddIcon />
+                </ProductAmountContainer>
+                <ProductPrice>₹799</ProductPrice>
+              </PriceDetail>
+            </Product>
+          </Info>
+          <Summary>
+            <SummaryTitle>Order Summary</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemTexts>Subtotal:</SummaryItemTexts>
+              <SummaryItemPrice>₹1598</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemTexts>Shipping Charges:</SummaryItemTexts>
+              <SummaryItemPrice>₹60</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemTexts>Shipping Discount</SummaryItemTexts>
+              <SummaryItemPrice>-₹60</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemTexts>Total</SummaryItemTexts>
+              <SummaryItemPrice>₹1598</SummaryItemPrice>
+            </SummaryItem>
+            <Button>CHECKOUT NOW</Button>
+          </Summary>
+        </Bottom>
+      </Wrapper>
+
+      <Footer />
+    </Container>
+  );
+};
+
+export default Cart;
