c5d3e3409b4458eb05e29f141538c181770ed1dc
Arnab Ghosh <arnabbabon2002@gmail.com>
2024-04-08 00:07:28 +0530

    finished user controller and routes

--- Backend/src/middleWares/verifyToken.js
+++ Backend/src/middleWares/verifyToken.js
@@ -17,7 +17,7 @@ const verifyToken = (req, res, next) => {
 
 const verifyTokenAndAuthorization = (req, res, next) => {
   verifyToken(req, res, () => {
-    if (req.user.id === req.params.id) {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
       next();
     } else {
       return res.status(403).json({ error: "You are not allowed!" });
@@ -25,4 +25,14 @@ const verifyTokenAndAuthorization = (req, res, next) => {
   });
 };
 
-export { verifyToken, verifyTokenAndAuthorization };
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      return res.status(403).json({ error: "You are not allowed!" });
+    }
+  });
+};
+
+export { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin };
--- Backend/src/routers/user.routes.js
+++ Backend/src/routers/user.routes.js
@@ -1,6 +1,7 @@
-import { Router } from "express";
+import e, { Router } from "express";
 import {
   verifyToken,
+  verifyTokenAndAdmin,
   verifyTokenAndAuthorization,
 } from "../middlewares/verifyToken.js"; // corrected import path
 import CryptoJS from "crypto-js";
@@ -8,24 +9,88 @@ import { User } from "../models/user.model.js";
 
 const router = Router();
 
-// update password
-router.put("/updatedetails/:id", verifyTokenAndAuthorization, async (req, res) => {
-  if (req.body.password) {
-    const newPassword = CryptoJS.AES.encrypt(
-      req.body.password, 
-      process.env.PASSWORD_SECRET
-    ).toString();
-    req.body.password = newPassword;
-  }
+// update details
+router
+  .route("/updatedetails/:id")
+  .put(verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+      const newPassword = CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.PASSWORD_SECRET
+      ).toString();
+      req.body.password = newPassword;
+    }
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+
+// delete
+router
+  .route("/delete-profile/:id")
+  .delete(verifyTokenAndAuthorization, async (req, res) => {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User has been deleted.");
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+
+// get user profile
+router.route("/your-profile/:id").get(verifyTokenAndAdmin, async (req, res) => {
   try {
-    const updatedUser = await User.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: req.body,
-      },
-      { new: true }
+    const user = await User.findById(user._id).select(
+      "-password -refreshToken"
     );
-    res.status(200).json(updatedUser);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// get all user profile
+router.route("/all-profile").get(verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
+
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// get user stats
+
+router.route("/stats").get(verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },{$group:{
+        _id:"$month",
+        total:{$sum}
+      }}
+    ]);
+    res.sendStatus(200).json(data);
   } catch (error) {
     res.status(500).json(error);
   }
