c94911cc2a75628e9a4f11d3fa0dd3d6f659dea2
Arnab Ghosh <arnabbabon2002@gmail.com>
2024-04-12 03:41:00 +0530

    update token and slice

--- Frontend/src/requestMethod.js
+++ Frontend/src/requestMethod.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:1328/eazybuy/v1";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MTJiMTE5NDAxZTA4ZmYwNWUzNmJjMCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcxMjc0NzI1NSwiZXhwIjoxNzEzMDA2NDU1fQ.g6cMO4xIF58NFTzjFNkCzYIZWlfIxvyi8RuAeeFIDrc";
-
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.accessToken;
+  
 export const publicRequest = axios.create({ baseURL: BASE_URL });
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  header: { token: `Bearer ${TOKEN}` },
+  headers: { token: `Bearer ${TOKEN}` },
 });
