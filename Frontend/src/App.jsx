d57dabc8bdf67d1f45ed8dc3e36dc40ef3b23226
Arnab Ghosh <arnabbabon2002@gmail.com>
2024-04-11 15:13:34 +0530

    path added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,12 +4,29 @@ import Product from "./Pages/Product";
 import ProductList from "./Pages/ProductList";
 import Register from "./Pages/Register";
 import Cart from "./Pages/Cart";
+import Success from "./Pages/Success";
+import { useSelector } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Navigate,
+  Route,
+} from "react-router-dom";
 
 function App() {
+  const user = useSelector((state) => state.user.currentUser);
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/productlist/:category" element={<ProductList />} />
+      </Routes>
+    </Router>
   );
 }
 
