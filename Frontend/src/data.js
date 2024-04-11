9c164c0e4c5bcfaf20d018f3ca67f7b6413b4211
Arnab Ghosh <arnabbabon2002@gmail.com>
2024-04-11 15:07:14 +0530

    dummy data update

--- Frontend/src/data.js
+++ Frontend/src/data.js
@@ -1,91 +1,79 @@
 export const sliderItems = [
   {
     id: 1,
-    img: "https://w7.pngwing.com/pngs/503/715/png-transparent-shopping-graphy-shopping-photography-people-shopping-centre-thumbnail.png",
+    img: "https://i.ibb.co/XsdmR2c/1.png",
     title: "SUMMER SALE",
-    description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
-    bg: "fcf1ed",
-  },
-  {
-    id: 2,
-    img: "https://w7.pngwing.com/pngs/503/715/png-transparent-shopping-graphy-shopping-photography-people-shopping-centre-thumbnail.png",
-    title: "WINTER SALE",
-    description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "f5fafd",
   },
   {
+    id: 2,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
     id: 3,
-    img: "https://w7.pngwing.com/pngs/503/715/png-transparent-shopping-graphy-shopping-photography-people-shopping-centre-thumbnail.png",
-    title: "AUTUMN SALE",
-    description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
-    bg: "fcf1ed",
-  },
-  {
-    id: 4,
-    img: "https://w7.pngwing.com/pngs/503/715/png-transparent-shopping-graphy-shopping-photography-people-shopping-centre-thumbnail.png",
-    title: "SPRING SALE",
-    description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    img: "https://i.ibb.co/cXFnLLV/3.png",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "fbf0f4",
-  },
-  {
-    id: 5,
-    img: "https://w7.pngwing.com/pngs/503/715/png-transparent-shopping-graphy-shopping-photography-people-shopping-centre-thumbnail.png",
-    title: "FESTIVE SALE",
-    description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
-    bg: "fcf1ed",
   },
 ];
 
 export const categories = [
   {
     id: 1,
-    img: "https://w7.pngwing.com/pngs/494/681/png-transparent-orange-crew-neck-shirt-t-shirt-hoodie-sleeve-clothing-t-shirt-tshirt-orange-fashion-thumbnail.png",
-    title: "T-SHIRT",
+    img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "SHIRT STYLE!",
+    cat:"woman"
   },
   {
     id: 2,
-    img: "https://w7.pngwing.com/pngs/827/140/png-transparent-t-shirt-fashion-clothing-mens-fashion-tshirt-abdomen-formal-wear-thumbnail.png",
-    title: "SHIRTS",
+    img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "LOUNGEWEAR LOVE",
+    cat:"coat"
   },
   {
     id: 3,
-    img: "https://w7.pngwing.com/pngs/982/812/png-transparent-little-black-dress-clothing-cocktail-dress-skirt-women-with-women-accessories-holidays-atmosphere-thumbnail.png",
-    title: "WOMEN WEAR",
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    title: "LIGHT JACKETS",
+    cat:"jeans"
   },
 ];
-
 
 export const popularProducts = [
   {
     id:1,
-    img:"https://w7.pngwing.com/pngs/297/928/png-transparent-women-s-maroon-leather-2-way-bag-handbag-messenger-bag-leather-tote-bag-women-s-handbags-purple-luggage-bags-holidays-thumbnail.png"
+    img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
   },
   {
     id:2,
-    img:"https://w7.pngwing.com/pngs/297/928/png-transparent-women-s-maroon-leather-2-way-bag-handbag-messenger-bag-leather-tote-bag-women-s-handbags-purple-luggage-bags-holidays-thumbnail.png"
+    img:"https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
   },
   {
     id:3,
-    img:"https://w7.pngwing.com/pngs/297/928/png-transparent-women-s-maroon-leather-2-way-bag-handbag-messenger-bag-leather-tote-bag-women-s-handbags-purple-luggage-bags-holidays-thumbnail.png"
+    img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
   },
   {
     id:4,
-    img:"https://w7.pngwing.com/pngs/297/928/png-transparent-women-s-maroon-leather-2-way-bag-handbag-messenger-bag-leather-tote-bag-women-s-handbags-purple-luggage-bags-holidays-thumbnail.png"
+    img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
   },
   {
     id:5,
-    img:"https://w7.pngwing.com/pngs/297/928/png-transparent-women-s-maroon-leather-2-way-bag-handbag-messenger-bag-leather-tote-bag-women-s-handbags-purple-luggage-bags-holidays-thumbnail.png"
+    img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
   },
   {
     id:6,
-    img:"https://w7.pngwing.com/pngs/297/928/png-transparent-women-s-maroon-leather-2-way-bag-handbag-messenger-bag-leather-tote-bag-women-s-handbags-purple-luggage-bags-holidays-thumbnail.png"
+    img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
   },
   {
     id:7,
-    img:"https://w7.pngwing.com/pngs/297/928/png-transparent-women-s-maroon-leather-2-way-bag-handbag-messenger-bag-leather-tote-bag-women-s-handbags-purple-luggage-bags-holidays-thumbnail.png"
+    img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
   },
   {
     id:8,
-    img:"https://w7.pngwing.com/pngs/297/928/png-transparent-women-s-maroon-leather-2-way-bag-handbag-messenger-bag-leather-tote-bag-women-s-handbags-purple-luggage-bags-holidays-thumbnail.png"
+    img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
   },
 ]
