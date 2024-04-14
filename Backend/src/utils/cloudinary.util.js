e0cfeb58ce47f5f0b7f0fcddf728b31252dc4d06
Arnab Ghosh <arnabbabon2002@gmail.com>
2024-04-14 19:11:42 +0530

    added cloudinary

---
+++ Backend/src/utils/cloudinary.util.js
@@ -0,0 +1,40 @@
+import { v2 as cloudinary } from "cloudinary";
+import { extractPublicId } from "cloudinary-build-url";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (filePath, cloudinaryFolder) => {
+  try {
+    if (!filePath) return null;
+    const response = await cloudinary.uploader.upload(filePath, {
+      folder: cloudinaryFolder,
+      resource_type: "auto",
+    });
+    fs.unlinkSync(filePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(filePath);
+    return null;
+  }
+};
+
+const deleteFromCloudinary = async (url, resourceType = "image") => {
+  const publicId = extractPublicId(url);
+  try {
+    const response = await cloudinary.uploader.destroy(publicId, {
+      resource_type: resourceType,
+    });
+    return response;
+  } catch (error) {
+    console.log("Error while deleting from cloudinary");
+    console.log(error);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary, deleteFromCloudinary };
