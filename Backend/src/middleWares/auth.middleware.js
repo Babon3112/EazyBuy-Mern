907a34bc627a834da8ad6795d777815c21a440dc
Arnab Ghosh <arnabbabon2002@gmail.com>
2024-04-16 11:01:03 +0530

    make verify admin and authorization optimized

--- Backend/src/middleWares/auth.middleware.js
+++ Backend/src/middleWares/auth.middleware.js
@@ -3,39 +3,53 @@ import { ApiError } from "../utils/ApiError.util.js";
 import { asyncHandler } from "../utils/asyncHandler.util.js";
 import jwt from "jsonwebtoken";
 
-export const verifyJWT = asyncHandler(async (req, res, next) => {
-  try {
-    const token =
-      req.cookies?.accessToken ||
-      req.header("Authorization")?.replace("Bearer ", "");
+const verifyJWT = async (req) => {
+  const token =
+    req.cookies?.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
 
-    if (!token) {
-      throw new ApiError(401, "Unauthorized request");
+  if (!token) {
+    throw new ApiError(401, "Unauthorized request");
+  }
+
+  const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+  const user = await User.findById(decodedToken?._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!user) {
+    throw new ApiError(401, "Invalid Access Token");
+  }
+
+  return user;
+};
+
+export const verifyJWTAndAuthorization = asyncHandler(
+  async (req, res, next) => {
+    try {
+      req.user = await verifyJWT(req);
+      next();
+    } catch (error) {
+      throw new ApiError(401, error?.message || "Invalid Access Token");
     }
+  }
+);
 
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+export const verifyJWTAndAdmin = asyncHandler(async (req, res, next) => {
+  try {
+    const user = await verifyJWT(req);
 
-    const user = await User.findById(decodedToken?._id).select(
-      "-password -refreshToken"
-    );
-
-    if (!user) {
-      throw new ApiError(401, "Invalid Access Token");
+    if (!user.isAdmin) {
+      throw new ApiError(403, "You are not Authorized!");
     }
 
     req.user = user;
     next();
   } catch (error) {
-    throw new ApiError(401, error?.message || "Invalid Access Token");
+    throw new ApiError(
+      error?.status || 401,
+      error?.message || "Invalid Access Token"
+    );
   }
 });
-
-export const verifyJWTAndAdmin = (req, res, next) => {
-  verifyJWT(req, res, () => {
-    if (req.user.isAdmin) {
-      next();
-    } else {
-      throw new ApiError(401, "You are not Authorized");
-    }
-  });
-};
