ba19b428b33c8f048f08cc84ae8c86c700bc3ee5
Arnab Ghosh <arnabbabon2002@gmail.com>
2024-04-17 11:13:18 +0530

    upadate details and delete account apicall added

--- Frontend/src/redux/apiCalls.js
+++ Frontend/src/redux/apiCalls.js
@@ -1,21 +1,27 @@
 import { publicRequest, userRequest } from "../requestMethod";
 import {
+  registerStart,
+  registerSuccess,
+  registerFailure,
   loginFailure,
   loginStart,
   loginSuccess,
   logOut,
-  registerUserStart,
-  registerUserSuccess,
-  registerUserFailure,
+  updateStart,
+  updateSuccess,
+  updateFailure,
+  deleteStart,
+  deleteSuccess,
+  deleteFailure,
 } from "./userRedux";
 
 export const register = async (dispatch, user) => {
-  dispatch(registerUserStart());
+  dispatch(registerStart());
   try {
     const res = await publicRequest.post("/users/register", user);
-    dispatch(registerUserSuccess(res.data));
+    dispatch(registerSuccess(res.data));
   } catch (error) {
-    dispatch(registerUserFailure());
+    dispatch(registerFailure());
   }
 };
 
@@ -34,3 +40,22 @@ export const logout = async (dispatch, user) => {
   dispatch(logOut());
 };
 
+export const updateAccount = async (dispatch, user) => {
+  dispatch(updateStart());
+  try {
+    const res = await userRequest.patch("/users/update-details", user);
+    dispatch(updateSuccess(res.data.data));
+  } catch (error) {
+    dispatch(updateFailure());
+  }
+};
+
+export const deleteAccount = async (dispatch, password) => {
+  dispatch(deleteStart());
+  try {
+    const res = await userRequest.delete("/users/delete-account", { data: { password } });
+    dispatch(deleteSuccess(res.data.data));
+  } catch (error) {
+    dispatch(deleteFailure());
+  }
+};
